64e357c6aa932dd47800def1547de31024376763
leomilitz <39914866+leomilitz@users.noreply.github.com>
2019-03-24 18:41:57 -0300

    Add files via upload

--- t1/t1.hs
+++ t1/t1.hs
@@ -1,16 +1,18 @@
 import Data.Char
 
--- 1.
+-- 1. Crie uma função isVowel :: Char -> Bool que verifique se um caracter é uma vogal ou não.
 
 isVowel :: Char -> Bool
 isVowel r = length (filter (\x -> x == r) "aeiouAEIOU") > 0
 
--- 2.
+-- 2. Escreva uma função addComma, que adicione uma vírgula no final de cada string contida numa lista.
 
 addComma :: [String] -> [String]
 addComma list = map (++ ",")list
 
--- 3.
+-- 3. Crie uma função htmlListItems :: [String] -> [String], que receba uma lista de strings e retorne 
+-- outra lista contendo as strings formatadas como itens de lista em HTML. Resolva este exercício COM 
+-- e SEM funções anônimas (lambda). 
 
 --com lambda
 htmlListItems :: [String] -> [String]
@@ -24,8 +26,8 @@ htmlListItemsSL list = map htmlListItemsSLAux list
 htmlListItemsSLAux :: String -> String
 htmlListItemsSLAux str = "<LI>" ++ str ++ "</LI>"
 
-
--- 4.
+-- 4. Defina uma função que receba uma string e produza outra retirando as vogais, conforme os exemplos
+-- abaixo. Resolva este exercício COM e SEM funções anônimas (lambda).
 
 -- Sem Lambda
 
@@ -37,7 +39,9 @@ noVowelSL r = filter (`notElem` "aeiouAEIOU") r
 noVowelsCL :: String -> String
 noVowelsCL str = filter (\r -> length (filter (\x -> x == r) "aeiouAEIOU") == 0) str  
 
--- 5.
+-- 5. Defina uma função que receba uma string, possivelmente contendo espaços, e que retorne outra string
+--  substituindo os demais caracteres por '-', mas mantendo os espaços. Resolva este exercício COM e SEM 
+-- funções anônimas (lambda). 
 
 -- Com lambda
 
@@ -52,22 +56,27 @@ putHyphen2 string =  map putHyphen2Aux string
 putHyphen2Aux :: Char -> Char
 putHyphen2Aux c = if c /= ' ' then '-' else c
 
--- 6.
+-- 6. Escreva uma função firstName :: String -> String que, dado o nome completo de uma pessoa, obtenha seu 
+-- primeiro nome. Suponha que cada parte do nome seja separada por um espaço e que não existam espaços no 
+-- início ou fim do nome. 
 
 firstName :: String -> String
 firstName str = takeWhile (/= ' ') str
 
--- 7. 
+-- 7. Escreva uma função isInt :: String -> Bool que verifique se uma dada string só contém dígitos de 0 a 9. 
 
 isInt :: String -> Bool
 isInt str = length (filter isDigit str) > 0
 
--- 8.
+-- 8. Escreva uma função lastName :: String -> String que, dado o nome completo de uma pessoa, obtenha seu 
+-- último sobrenome. Suponha que cada parte do nome seja separada por um espaço e que não existam espaços 
+-- no início ou fim do nome.
 
 lastName :: String -> String
 lastName str = filter (/= ' ') (dropWhile (/= ' ') str)
 
--- 9.
+-- 9. Escreva uma função userName :: String -> String que, dado o nome completo de uma pessoa, crie um nome de
+--  usuário (login) da pessoa, formado por: primeira letra do nome seguida do sobrenome, tudo em minúsculas.
 
 userName :: String -> String
 userName name = map toLower ([head name] ++ lastName name) 
@@ -104,7 +113,9 @@ betterEncodeName2 c
     | c == 'u' = "00"
     | otherwise = [c]
 
--- 12. 
+-- 12. Dada uma lista de strings, produzir outra lista com strings de 10 caracteres, usando o seguinte esquema:
+--  strings de entrada com mais de 10 caracteres são truncadas, strings com até 10 caracteres são completadas com
+--  '.' até ficarem com 10 caracteres.
 
 limitTen :: [String] -> [String]
 limitTen list = map (take 10) (map (++ "..........") list)
