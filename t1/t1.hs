86457b318bef1bda8b411548d980838bf8284dc8
leomilitz <39914866+leomilitz@users.noreply.github.com>
2019-03-24 18:23:26 -0300

    Add files via upload

--- t1/t1.hs
+++ t1/t1.hs
@@ -71,3 +71,19 @@ lastName str = filter (/= ' ') (dropWhile (/= ' ') str)
 
 userName :: String -> String
 userName name = map toLower ([head name] ++ lastName name) 
+
+-- 10. Escreva uma função encodeName :: String -> String 
+-- que substitua vogais em uma string, conforme o esquema a seguir:
+--  a = 4, e = 3, i = 2, o = 1, u = 0.
+
+encodeName :: String -> String 
+encodeName str = map encodeName2 (map toLower str)
+
+encodeName2 :: Char -> Char
+encodeName2 c
+    | c == 'a' = '4' 
+    | c == 'e' = '3'
+    | c == 'i' = '2'
+    | c == 'o' = '1'
+    | c == 'u' = '0'
+    | otherwise = c
