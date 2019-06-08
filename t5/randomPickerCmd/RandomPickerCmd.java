8218c77f03396ce8d85fb02832be3ef124cd0cdc
Leonardo Militz <39914866+leomilitz@users.noreply.github.com>
2019-06-08 16:34:07 -0300

    Update RandomPickerCmd.java

--- t5/randomPickerCmd/RandomPickerCmd.java
+++ t5/randomPickerCmd/RandomPickerCmd.java
@@ -9,34 +9,34 @@ class RandomPickerCmd {
 	public static void main(String[] args) {	
 		try {
 			File file = new File(args[0]);
-		    Scanner scanner = new Scanner(file);
-	      	Scanner sysPause = new Scanner(System.in); // scanner usado para pausar a execução no console
+		    	Scanner scanner = new Scanner(file);
+	      		Scanner sysPause = new Scanner(System.in); // scanner usado para pausar a execução no console
 			ArrayList<String> nameList = new ArrayList<String>();
 			
 			// Adiciona os elementos do txt no array list
 			while (scanner.hasNextLine()) {
-	        	nameList.add(scanner.nextLine()); 
-	      	}
+	        		nameList.add(scanner.nextLine()); 
+	      		}
 
-	      	Collections.shuffle(nameList); // Embaralha a lista de elementos
+	      		Collections.shuffle(nameList); // Embaralha a lista de elementos
 
-	      	// Imprime os nomes embaralhados um por um
-	      	System.out.println("\nShuffled " + args[0] + ":\n");
-	      	for (int i=0 ; i<nameList.size() ; i++) {
+	      		// Imprime os nomes embaralhados um por um
+	      		System.out.println("\nShuffled " + args[0] + ":\n");
+	      		for (int i=0 ; i<nameList.size() ; i++) {
 	      		
-	      		if (i < nameList.size() - 1)
-	      			System.out.println("\t- " + nameList.get(i) + ";");
-	      		else
-	      			System.out.println("\t- " + nameList.get(i) + ".");
+	      			if (i < nameList.size() - 1)
+	      				System.out.println("\t- " + nameList.get(i) + ";");
+	      			else
+	      				System.out.println("\t- " + nameList.get(i) + ".");
 
-	      		sysPause.nextLine();
-	      	}
+	      			sysPause.nextLine();
+	      		}
 
-	      	scanner.close();
-	    }
-	    catch (FileNotFoundException e) {
-      		System.out.println("An error occurred.");
-      		e.printStackTrace();
-    	} 
-	}
+	      		scanner.close();
+	    	}
+	    	catch (FileNotFoundException e) {
+      			System.out.println("An error occurred.");
+      			e.printStackTrace();
+    	    	} 
+    	}
 }
